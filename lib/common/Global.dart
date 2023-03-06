a4e849d40f385cfd9d476c1e204285886d8c3d20
Wang YiMing <wangyimig@icloud.com>
2023-03-06 21:50:58 +0800

    feat: 封装全局共享类和方法

---
+++ lib/common/Global.dart
@@ -0,0 +1,145 @@
+
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../models/cacheConfig.dart';
+import '../models/profile.dart';
+import 'GitHub.dart';
+
+// 提供五套可选主题色
+const _themes = <MaterialColor>[
+  Colors.blue,
+  Colors.cyan,
+  Colors.teal,
+  Colors.green,
+  Colors.red,
+];
+
+class Global {
+  static late SharedPreferences _prefs;
+  static Profile profile = Profile();
+
+  // 网络缓存对象
+  static NetCache netCache = NetCache();
+
+  // 可选的主题列表
+  static List<MaterialColor> get themes => _themes;
+
+  // 是否为release版
+  static bool get isRelease => const bool.fromEnvironment("dart.vm.product");
+
+  //初始化全局信息，会在APP启动时执行
+  static Future init() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    _prefs = await SharedPreferences.getInstance();
+    var _profile = _prefs.getString("profile");
+    if (_profile != null) {
+      try {
+        profile = Profile.fromJson(jsonDecode(_profile));
+      } catch (e) {
+        print(e);
+      }
+    } else {
+      // 默认主题索引为0，代表蓝色
+      profile = Profile()..theme = 0;
+    }
+
+    // 如果没有缓存策略，设置默认缓存策略
+    profile.cache = profile.cache ?? CacheConfig()
+      ..enable = true
+      ..maxAge = 3600
+      ..maxCount = 100;
+
+    //初始化网络请求相关配置
+    GitHub.init();
+  }
+
+  // 持久化Profile信息
+  static saveProfile() =>
+      _prefs.setString("profile", jsonEncode(profile.toJson()));
+}
+
+class CacheObject {
+  CacheObject(this.response)
+      : timeStamp = DateTime.now().millisecondsSinceEpoch;
+  Response response;
+  int timeStamp;
+
+  @override
+  bool operator ==(other) {
+    return response.hashCode == other.hashCode;
+  }
+
+  @override
+  int get hashCode => response.realUri.hashCode;
+}
+
+class NetCache extends Interceptor {
+  // 为确保迭代器顺序和对象插入时间一致顺序一致，我们使用LinkedHashMap
+  var cache = <String, CacheObject>{};
+
+  @override
+  onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
+    if (!Global.profile.cache!.enable) {
+      return handler.next(options);
+    }
+    // refresh标记是否是"下拉刷新"
+    bool refresh = options.extra["refresh"] == true;
+    //如果是下拉刷新，先删除相关缓存
+    if (refresh) {
+      if (options.extra["list"] == true) {
+        //若是列表，则只要url中包含当前path的缓存全部删除（简单实现，并不精准）
+        cache.removeWhere((key, v) => key.contains(options.path));
+      } else {
+        // 如果不是列表，则只删除uri相同的缓存
+        delete(options.uri.toString());
+      }
+      return handler.next(options);
+    }
+    if (options.extra["noCache"] != true &&
+        options.method.toLowerCase() == 'get') {
+      String key = options.extra["cacheKey"] ?? options.uri.toString();
+      var ob = cache[key];
+      if (ob != null) {
+        //若缓存未过期，则返回缓存内容
+        if ((DateTime.now().millisecondsSinceEpoch - ob.timeStamp) / 1000 <
+            Global.profile.cache!.maxAge) {
+          return handler.resolve(ob.response);
+        } else {
+          //若已过期则删除缓存，继续向服务器请求
+          cache.remove(key);
+        }
+      }
+    }
+    handler.next(options);
+  }
+
+  @override
+  onResponse(Response response, ResponseInterceptorHandler handler) async {
+    // 如果启用缓存，将返回结果保存到缓存
+    if (Global.profile.cache!.enable) {
+      _saveCache(response);
+    }
+    handler.next(response);
+  }
+
+  _saveCache(Response object) {
+    RequestOptions options = object.requestOptions;
+    if (options.extra["noCache"] != true &&
+        options.method.toLowerCase() == "get") {
+      // 如果缓存数量超过最大数量限制，则先移除最早的一条记录
+      if (cache.length == Global.profile.cache!.maxCount) {
+        cache.remove(cache[cache.keys.first]);
+      }
+      String key = options.extra["cacheKey"] ?? options.uri.toString();
+      cache[key] = CacheObject(object);
+    }
+  }
+
+  void delete(String key) {
+    cache.remove(key);
+  }
+}
